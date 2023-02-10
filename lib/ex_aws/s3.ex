1228d990c9bd1bd8de4c7eac0bbee2a45af3e0e8
DavidSnoble <snoble1353@gmail.com>
2023-02-10 10:09:49 -0700

    rename

--- lib/ex_aws/s3.ex
+++ lib/ex_aws/s3.ex
@@ -825,7 +825,7 @@ defmodule ExAws.S3 do
   @spec head_object(bucket :: binary, object :: binary) :: ExAws.Operation.S3.t()
   @spec head_object(bucket :: binary, object :: binary, opts :: head_object_opts) ::
           ExAws.Operation.S3.t()
-  @request_headers [:range, :if_modified_since, :if_unmodified_since, :if_match, :if_none_match, :checksum_mode]
+  @request_headers [:range, :if_modified_since, :if_unmodified_since, :if_match, :if_none_match, :x_amz_checksum_mode]
   def head_object(bucket, object, opts \\ []) do
     opts = opts |> Map.new()
     @request_headsers |> IO.inspect(label: "DEBUG THIS REQUEST HEADER")
